62b6b798b9cc1211b39b3971bac3b640d5e2fd2c
Steve Beaver <sbeaver@netgate.com>
2018-11-10 07:29:36 -0500

    gofmt cleanup

--- tnsrids_test.go
+++ tnsrids_test.go
@@ -17,12 +17,12 @@
 package main
 
 import (
-	"testing"
 	"reflect"
+	"testing"
 )
 
-// Creat a set of program option and ensure that they are combined in a manner that lets the command line
-// override hte config file, or use the defaults if none are providded
+// Creat a set of program options and ensure that they are combined in a manner that lets the command line
+// override the config file, or use the defaults if none are provided
 func TestMergeItems(t *testing.T) {
 
 	var tconfig Config
@@ -32,13 +32,13 @@ func TestMergeItems(t *testing.T) {
 	tconfig.addOption("host", "h", true, "Host name of TNSR instance (including protocol prefix", dfltHost)
 	tconfig.addOption("port", "p", true, "UDP port on which to listen for alert messages", dfltPort)
 
-	args :=     map[string]string{"host":"192.168.1.4", "port":"",     "show":"",   "reap":"yes"}
-	conf :=     map[string]string{"host":"192.168.1.4", "port":"4444", "show":"",   "reap":"no"}  
-	expected := map[string]string{"host":"192.168.1.4", "port":"4444", "show":"no", "reap":"yes" }
+	args := map[string]string{"host": "192.168.1.4", "port": "", "show": "", "reap": "yes"}
+	conf := map[string]string{"host": "192.168.1.4", "port": "4444", "show": "", "reap": "no"}
+	expected := map[string]string{"host": "192.168.1.4", "port": "4444", "show": "no", "reap": "yes"}
 
 	received := tconfig.mergeItems(args, conf)
 
-	if ! reflect.DeepEqual(expected, received) {
+	if !reflect.DeepEqual(expected, received) {
 		t.Errorf("mergeItems() failed. Expected %v but received %v", expected, received)
 	}
 }
@@ -46,9 +46,9 @@ func TestMergeItems(t *testing.T) {
 // Ensure that an IPv4 address is properly extracted from an alert string
 func TestFindIP(t *testing.T) {
 	var tests = []struct {
-		alert string
+		alert   string
 		address string
-	} {
+	}{
 		{"This alert contains 172.21.2.4", "172.21.2.4"},
 		{"This alert contains 192.168.12.14/22", "192.168.12.14"},
 		{"192.168.12.14:9090 is contained in this alert", "192.168.12.14"},
@@ -62,25 +62,26 @@ func TestFindIP(t *testing.T) {
 	}
 }
 
-// Tests the generation of hte next sequence number by creating an aclcache with various sequence number missing a number
-// at teh start of the list, in the middle, at the end, of when a sequence # > maxSeqNum
+// Tests the generation of the next sequence number by creating an aclcache with various sequence numbers, missing a number
+// at the start of the list, in the middle, at the end, or with a sequence # > maxSeqNum
 func TestGetNextSeqNum(t *testing.T) {
 	var tests = []struct {
-		s1 uint64
-		s2 uint64
-		s3 uint64
-		s4 uint64
+		s1   uint64
+		s2   uint64
+		s3   uint64
+		s4   uint64
 		next uint64
-	} {
-		{1,2,3,4,5},
-		{2,3,4,5,1},
-		{1,2,3,6,4},
-		{1,2,3,maxSeqNum+1, 4},
+	}{
+		{1, 2, 3, 4, 5},
+		{2, 3, 4, 5, 1},
+		{1, 2, 3, 6, 4},
+		{1, 2, 3, maxSeqNum + 1, 4},
 	}
 
 	for _, test := range tests {
 		var rule AAclRule
 		aclcache.AclRule = aclcache.AclRule[:0] // Clear the list of rules
+		// Add four rules with the seqence numbers specified in the test data
 		rule.Sequence = test.s1
 		rule.Action = "deny"
 		aclcache.AclRule = append(aclcache.AclRule, rule)
@@ -102,15 +103,16 @@ func TestGetNextSeqNum(t *testing.T) {
 	}
 }
 
-// Tests whether the presence of a rule in hte cache can be verified
+// Tests whether the presence of a rule in the cache can be verified
 func TestRuleExists(t *testing.T) {
 	aclcache.AclRule = aclcache.AclRule[:0] // Clear the list of rules
 	var rule AAclRule
+	var rule2 AAclRule
 
 	rule.DstIPPrefix = "192.168.1.100"
 	aclcache.AclRule = append(aclcache.AclRule, rule)
-	rule.SrcIPPrefix = "192.168.10.100"
-	aclcache.AclRule = append(aclcache.AclRule, rule)
+	rule2.SrcIPPrefix = "192.168.10.100"
+	aclcache.AclRule = append(aclcache.AclRule, rule2)
 
 	if ruleExists("172.2.2.2") {
 		t.Errorf("Host 172.2.2.2 should not exist, but it does")
